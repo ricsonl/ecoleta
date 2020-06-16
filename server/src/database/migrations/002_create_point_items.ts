4c173c3242ca14cf1aaea55ef74f0121bf578915
ricsonl <ricsonluizv@gmail.com>
2020-06-16 20:46:14 -0300

    .

--- server/src/database/migrations/002_create_point_items.ts
+++ server/src/database/migrations/002_create_point_items.ts
@@ -1,7 +1,21 @@
-export async function up() {
+import Knex from 'knex';
 
+export async function up(knex: Knex) {
+    return knex.schema.createTable('point_item', table => {
+        table.increments('id').primary();
+
+        table.integer('point_id')
+            .notNullable()
+            .references('id')
+            .inTable('points');
+
+        table.integer('item_id')
+            .notNullable()
+            .references('id')
+            .inTable('item');
+    });
 }
 
-export async function down() {
-
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_item');
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,59 @@
-import express from 'express';
+import express, { response } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*'); 
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+    const {
+        name,
+        email,
+        wpp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = req.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        wpp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = insertedIds[0];
+    
+    const pointItems = items.map((item_id: Number) => {
+        return {
+            item_id,
+            point_id,
+        };
+    })
+    console.log(pointItems);
+    await trx('point_item').insert(pointItems);
+
+    return res.json({ success: true });
 });
 
 export default routes;
